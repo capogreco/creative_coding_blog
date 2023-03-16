b57e1edb19c99b6427bc2c7f8c1285366aa99ed6
capogreco <thomas.capogreco@gmail.com>
2023-03-16 15:46:24 +1100

    first post: date style

--- main.tsx
+++ main.tsx
@@ -4,24 +4,13 @@ import blog, { ga, redirects, h } from "blog";
 
 blog({
   title: "Creative Coding Blog!",
-  description: "Javascript, Communities of Practice, & Posthumanism",
-  // header: <header>Your custom header</header>,
-  // section: <section>Your custom section</section>,
-  // footer: <footer>Your custom footer</footer>,
+  description: "Javascript, Critical Posthumanism, etc.",
   avatar: "./images/rmit_pfp.jpeg",
   avatarClass: "rounded-full",
   author: "Thomas Capogreco",
-
-  // middlewares: [
-
-    // If you want to set up Google Analytics, paste your GA key here.
-    // ga("UA-XXXXXXXX-X"),
-
-    // If you want to provide some redirections, you can specify them here,
-    // pathname specified in a key will redirect to pathname in the value.
-    // redirects({
-    //  "/hello_world.html": "/hello_world",
-    // }),
-
-  // ]
+  dateStyle: "dd-MM-yyyy",
+  dateFormat: (date) =>  new Intl.DateTimeFormat (
+      "en-GB", 
+      { dateStyle: "long" }
+    ).format (date),
 });
